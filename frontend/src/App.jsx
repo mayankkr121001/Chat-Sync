2d35d972550d37f96eeefbc1f0806671d3623c9e
mayankkr121001 <mayanksahu121001@gmail.com>
2024-11-01 16:11:12 +0530

    frontend deploy errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import HomePage from './components/HomePage.jsx'
 import ChatPage from './components/ChatPage.jsx'
 import VerifyEmail from './utils/VerifyEmail.jsx'
-import ProtectedRoutes from './utils/protectedRoutes.jsx'
+import ProtectedRoutes from './utils/ProtectedRoutes.jsx'
 import api from './interceptors/axios.js'
 
 function App() {
